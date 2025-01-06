a2bf1f8b380d882d16be78d468195c364f261085
Jongabee <104025249+Jongabee@users.noreply.github.com>
2025-01-06 17:49:55 -0300

    feat: wrap app with Provider, BrowserRouter, and ConfigProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import esES from 'antd/lib/locale/es_ES';
+import { Provider } from 'react-redux';
+import { ConfigProvider } from 'antd';
+import { store } from './redux/store.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <ConfigProvider locale={esES}>
+        <App />
+      </ConfigProvider>
+    </BrowserRouter>
+  </Provider>,
+);
